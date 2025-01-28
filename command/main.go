cb1b2f84725fc9f8728dfb047ab2188ad5c836a4
jeon <wk0012345@naver.com>
2025-01-28 22:29:14 +0900

    1

---
+++ command/main.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+var overdraftLimit = -500
+
+type BankAccount struct {
+	balance int
+}
+
+func (b *BankAccount) Deposit(amount int) {
+	b.balance += amount
+}
+
+func (b *BankAccount) Withdraw(amount int) bool {
+	if b.balance-amount >= overdraftLimit {
+		b.balance -= amount
+		return true
+	}
+	return false
+}
+
+type Action int
+
+const (
+	Deposit Action = iota
+	Withdraw
+)
+
+type BankAccountCommand struct {
+	account   *BankAccount
+	action    Action
+	amount    int
+	succeeded bool
+}
+
+func (b *BankAccountCommand) Call() {
+	switch b.action {
+	case Deposit:
+		b.account.Deposit(b.amount)
+		b.succeeded = true
+	case Withdraw:
+		b.account.Withdraw(b.amount)
+	}
+}
+
+func (b *BankAccountCommand) Undo() {
+	if !b.succeeded {
+		return
+	}
+	switch b.action {
+	case Deposit:
+		b.account.Withdraw(b.amount)
+	case Withdraw:
+		b.account.Deposit(b.amount)
+	}
+}
+
+func NewBankAccountCommand(account *BankAccount, action Action, amount int) *BankAccountCommand {
+	return &BankAccountCommand{account: account, action: action, amount: amount}
+}
+func main() {
+	ba := BankAccount{}
+	cmd := NewBankAccountCommand(&ba, Deposit, 100)
+	cmd.Call()
+	cmd.Call()
+	fmt.Println(ba)
+	cmd1 := NewBankAccountCommand(&ba, Withdraw, 100)
+	cmd1.Call()
+	fmt.Println(ba)
+	cmd.Undo()
+	fmt.Println(ba)
+}
